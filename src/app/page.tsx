e28b35f46720f7772f209e489c19e751e63e209b
Haidar <haidarprayoga9b@gmail.com>
2024-11-14 09:10:58 +0700

    Test2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl text-center font-bold text-slate-500">Dashboard</h1>
+    <div className="w-[100%] h-[100vh] bg-gray-300 block items-center">
+      <h1 className="text-3xl font-bold top-5 text-slate-900">Dashboard</h1>
+      <Button className="bg-blue-500">Learn</Button>
     </div>
   );
 }
